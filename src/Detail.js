e03d40588f2d03a7765300cec995967d3c1e8e69
mc2918 <tlsdud1375@gmail.com>
2021-01-26 14:30:15 +0900

    Add styled-component modules and use styled component

--- src/Detail.js
+++ src/Detail.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Detail(props) {
   // google
   let { id } = useParams(); // 사용자가 입력한 파라메터가 들어가있다. /:id 값 뒤로 오는 것들
-  let seletItem = props.shoes.find((item) => item.id = id);
-  //  찾은 상품  = props.shoes.find((상품) => 상품.id = id); 
-  console.log(seletItem.id);
   let history = useHistory(); 
+  let seletItem = props.shoes.find((item) => item.id = id);
+
+  // style component 이용 css를 미리 입혀놓은 컴포넌트 사용
+  let Box = styled.div`
+    flex: 1 auto;
+    padding : 20px;
+    text-align: center;
+  `;
+
+  let Title = styled.h4`
+    font-size: 30px;
+    color: ${ props => props.color }
+  `; 
+
+  // 컴포넌트가 많아지면 class가 겹칠일이 줄어든다. 편의성 이런건 잘 모르겠다.
 
   return (
     <div className="container">
     <div className="row">
+      <Box>
+        <Title color={'red'}>Detail</Title> 
+        <Title color="blue">Detail</Title> 
+        {/* color라는 props를 전달 */}
+      </Box>
       <div className="col-md-6">
         <img src={ 'https://codingapple1.github.io/shop/shoes' + seletItem.id + '.jpg' } width="100%" />
       </div>
