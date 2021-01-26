62c4a86998270d19c6d7112b240f2fb3d882f57a
mc2918 <tlsdud1375@gmail.com>
2021-01-26 15:10:50 +0900

    Add node-sass modules and Detaul.scss , _reset

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import './Detail.scss';
 
 function Detail(props) {
   // google
@@ -16,7 +17,6 @@ function Detail(props) {
   `;
 
   let Title = styled.h4`
-    font-size: 30px;
     color: ${ props => props.color }
   `; 
 
@@ -30,11 +30,14 @@ function Detail(props) {
         <Title color="blue">Detail</Title> 
         {/* color라는 props를 전달 */}
       </Box>
+      <div className="my-alert">
+        <p>재고가 많이 남지 않았음</p>
+      </div>
       <div className="col-md-6">
         <img src={ 'https://codingapple1.github.io/shop/shoes' + seletItem.id + '.jpg' } width="100%" />
       </div>
       <div className="col-md-6 mt-4">
-        <h4 className="pt-5">{seletItem.title}</h4>
+        <h4 className="pt-5 title-big">{seletItem.title}</h4>
         <p>{seletItem.content}</p>
         <p>{seletItem.price}</p>
         <button className="btn btn-danger">주문하기</button> 
@@ -44,9 +47,5 @@ function Detail(props) {
   </div> 
   )
 }
-// 자료 순서가 변경되면 상세페이지도 문제
-// 정렬이될 경우 데이터가 정렬의 기준으로 정렬이 되어버리는데 
-// 그럼 해당 id가 정렬된 순서로 상품이 변화가 되어버린다.
-// Datil.js  데이터 바인딩시 0 번째상품을 보여주세요가 아니라 상품의 고유 id의 0번째 상품을 보여줘야 한다는 것이다.
 
-export default Detail; // Detail이라는 것을 내뱉는 것 
+export default Detail;
