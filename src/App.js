2613a8d0118ad5e496ddb195eb6425c78a8d08cb
mc2918 <tlsdud1375@gmail.com>
2021-01-27 10:56:46 +0900

    Add axios modules and more button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { useState } from 'react';
 import Data from './data';
 import Detail from './Detail';
+import axios from 'axios';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -56,6 +57,27 @@ function App() {
               }
             </div>
           </div>
+          <button type="button" className="btn btn-primary" onClick={() => {
+            //  요청을 한다. -서버 요청 
+            // axios 쓰면 json 파일을 Object파일을 알아서 바꿔준다.
+            axios.get('https://codingapple1.github.io/shop/data2.json')
+            .then((result) => {
+              // 성공했을 때
+              // console.log('성공했다');
+              // console.log(result.data);
+              // 무조건 직접바꾸면 안된다. 카피본을 set할 경우 생성하고 작업을 해야한다는 점
+              // [...shoes] 카피본 생성 
+              setShoes([...shoes, ...result.data]);
+            })
+            .catch(() => {
+              // 실패했을때
+              console.log('실패했다.')
+            })
+
+            // javascript - 차이점 
+            // fetch('https://codingapple1.github.io/shop/data2.json').then()
+          
+          }}>더보기</button>
 
         </Route>
         {/* id: id */}
@@ -67,7 +89,7 @@ function App() {
           <div>아무 문자나 받는 URL 작성명 parameter 작성법</div>
         </Route>
 
-      </Switch>eslintcache
+      </Switch>
 
       {/* <Route path="/어쩌구"  component={Modal}></Route> */}
     </div>
