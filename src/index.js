e3f943ca92f63d281b882ceefd483baaf2bd730b
mc2918 <tlsdud1375@gmail.com>
2021-01-29 23:09:04 +0900

    Add homepage url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
